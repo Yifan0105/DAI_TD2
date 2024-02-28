3b9ac1299acde8ee6be9e996a15041d75fd42e1f
yuanfr123 <116172396+yuanfr123@users.noreply.github.com>
2024-02-28 17:01:07 +0100

    search

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -150,3 +150,31 @@
   }); // End of a document
 
 })(jQuery);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  var searchProduct = document.getElementById('searchValue');
+  searchProduct.addEventListener('click', function() {
+  let inputValue = document.getElementById('searchInput').value
+  if (inputValue) {
+      fetch(`http://localhost:8080/v1/products/search/${inputValue}`)
+      .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.text(); // Convertir la réponse en texte brut
+        })
+        .then(text => {
+          if (!text) {
+            throw new Error('Empty response text');
+          }
+          const data = JSON.parse(text);
+          console.log(data)
+        })
+        .catch(error => {
+          console.error('Erreur lors de la récupération des données :', error);
+        });
+          
+  }
+})
+})
